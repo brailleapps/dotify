8a15efcb27e522eac3866e2708f0df5843bae958
Joel Håkansson <joel.hakansson@mtm.se>
2011-05-17 06:55:23 +0000

    Updated FilterLocaleTest Added BreakPointHandlerTest

--- Dotify/tests/org/daisy/dotify/system/tasks/layout/text/FilterLocaleTest.java
+++ Dotify/tests/org/daisy/dotify/system/tasks/layout/text/FilterLocaleTest.java
@@ -10,34 +10,29 @@ public class FilterLocaleTest {
 		FilterLocale inLoc = FilterLocale.parse("sv-SE");
 		FilterLocale refLoc = FilterLocale.parse("sv");
 		assertTrue("Assert sv-SE is a sv", inLoc.isA(refLoc));
+		inLoc = FilterLocale.parse("sv-se-test");
+		refLoc = FilterLocale.parse("sv-SE-test");
+		assertTrue("Assert sv-SE-test is a sv-SE-test", inLoc.isA(refLoc));
 	}
 	
 	@Test
-	public void testIsNotA_1() {
+	public void testIsNotA() {
 		FilterLocale inLoc = FilterLocale.parse("sv-FI");
 		FilterLocale refLoc = FilterLocale.parse("da-DK");
 		assertTrue("Assert sv-FI is not a da-DK", !inLoc.isA(refLoc));
-	}
-	
-	@Test
-	public void testIsNotA_2() {
-		FilterLocale inLoc = FilterLocale.parse("sv-SE");
-		FilterLocale refLoc = FilterLocale.parse("sv-SE-test");
+		inLoc = FilterLocale.parse("sv-SE");
+		refLoc = FilterLocale.parse("sv-SE-test");
 		assertTrue("Assert sv-SE is not a sv-SE-test", !inLoc.isA(refLoc));
-	}
-	
-	@Test
-	public void testIsNotA_3() {
-		FilterLocale inLoc = FilterLocale.parse("sv");
-		FilterLocale refLoc = FilterLocale.parse("da");
+		inLoc = FilterLocale.parse("sv");
+		refLoc = FilterLocale.parse("da");
 		assertTrue("Assert sv is not a da", !inLoc.isA(refLoc));
 	}
-
+	
 	@Test
-	public void testSameIsA() {
+	public void testEquals() {
 		FilterLocale inLoc = FilterLocale.parse("sv-SE-test");
-		FilterLocale refLoc = FilterLocale.parse("sv-SE-test");
-		assertTrue("Assert sv-SE-test is a sv-SE-test", inLoc.isA(refLoc));
+		FilterLocale refLoc = FilterLocale.parse("sv-se-test");
+		assertTrue("Assert sv-SE-test is a sv-SE-test", inLoc.equals(refLoc));
 	}
 
 }
