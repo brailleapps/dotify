71a2e1a2e341f5035cd75879f0ff9f14ddf0e16f
Joel Håkansson <joel.hakansson@mtm.se>
2012-10-01 12:07:29 +0000

    performance improvement

--- DotifyTranslator/src/org/daisy/dotify/translator/sv_SE/CapitalizationMarkers.java
+++ DotifyTranslator/src/org/daisy/dotify/translator/sv_SE/CapitalizationMarkers.java
@@ -1,5 +1,7 @@
 package org.daisy.dotify.translator.sv_SE;
 
+import java.util.regex.Pattern;
+
 import org.daisy.dotify.text.SplitResult;
 import org.daisy.dotify.text.StringFilter;
 import org.daisy.dotify.text.StringSplitter;
@@ -16,6 +18,21 @@ class CapitalizationMarkers implements StringFilter {
 	private final static String WORD_PART_POSTFIX = "\u2831";
 	private final static String SEQ_PREFIX_MARKER = "\u2820\u2820\u2820";
 	private final static String SEQ_POSTFIX_MARKER = "\u2831";
+	private final Pattern p1;
+	private final Pattern p2;
+	private final Pattern p3;
+	private final Pattern p4;
+	private final Pattern p5;
+	private final Pattern p6;
+	
+	public CapitalizationMarkers() {
+		this.p1 = Pattern.compile("(?<=[^\\p{L}]|\\A)(((\\p{Lu}(\u00ad)?)+[\\s\\-/]+)+(\\p{Lu}(\u00ad)?)+)(?=[^\\p{L}^\u283c^\\d]|\\z)");
+		this.p2 = Pattern.compile("([\\p{Lu}][\\s]+)+[\\p{Lu}]");
+		this.p3 = Pattern.compile("(\\p{Lu})");
+		this.p4 = Pattern.compile("[\\p{L}[\\-\\d\u00ad]]+");
+		this.p5 = Pattern.compile("\\A(\\p{Lu}(\u00ad)?){2,}\\z");
+		this.p6 = Pattern.compile("(\\A|(?<=\\-))(\\p{Lu}(\u00ad)?)+");
+	}
 
 	public String filter(String str) {
 		return addCapitalizationMarkers(str);
@@ -27,12 +44,12 @@ class CapitalizationMarkers implements StringFilter {
 		// if preceded by beginning of input or any non letter character and
 		// followed by end of input or any non letter character except a digit or 0x283c
 		//^\\-^/^\\d
-		for (SplitResult sr : StringSplitter.split(input, "(?<=[^\\p{L}]|\\A)(((\\p{Lu}(\u00ad)?)+[\\s\\-/]+)+(\\p{Lu}(\u00ad)?)+)(?=[^\\p{L}^\u283c^\\d]|\\z)")) {
+		for (SplitResult sr : StringSplitter.split(input, p1)) {
 			String s = sr.getText();
 			if (sr.isMatch()) {
-				if (s.matches("([\\p{Lu}][\\s]+)+[\\p{Lu}]")) {
+				if (p2.matcher(s).matches()) {
 					// String is a group of single capital letters, e.g: 'E X A M P L E'
-					ret.append(s.replaceAll("(\\p{Lu})",CHAR_MARKER + "$1"));
+					ret.append(p3.matcher(s).replaceAll(CHAR_MARKER + "$1"));
 				} else {
 					// String is a group of capitalized words.
 					String asGroup = markAsGroup(s);
@@ -61,16 +78,16 @@ class CapitalizationMarkers implements StringFilter {
 	private String markAsWords(String s) {
 		StringBuffer ret = new StringBuffer();
 		// Split on words, or word like character groups (such as passwords)
-		for (SplitResult tr : StringSplitter.split(s, "[\\p{L}[\\-\\d\u00ad]]+")) {
+		for (SplitResult tr : StringSplitter.split(s, p4)) {
 			String t = tr.getText();
 			if (tr.isMatch()) {
 				// String is a word, e.g. 'hello', 'pqr6XWr', 'ISBN-centralen'
-				if (t.matches("\\A(\\p{Lu}(\u00ad)?){2,}\\z")) {
+				if (p5.matcher(t).matches()) {
 					// String is a single capitalized word longer than one letter, e.g. 'OK'
 					ret.append(WORD_MARKER+t);
 				} else {
 					// String contains non upper case letters or other characters
-					for (SplitResult ur : StringSplitter.split(t, "(\\A|(?<=\\-))(\\p{Lu}(\u00ad)?)+")) {
+					for (SplitResult ur : StringSplitter.split(t, p6)) {
 						String u = ur.getText();
 						if (ur.isMatch() && u.length()>2) {
 							// Input begins with upper case letters
@@ -79,7 +96,7 @@ class CapitalizationMarkers implements StringFilter {
 							ret.append(WORD_PART_POSTFIX);
 						} else {
 							// Use a single upper case mark for all upper case letters
-							ret.append(u.replaceAll("(\\p{Lu})", CHAR_MARKER+"$1"));
+							ret.append(p3.matcher(u).replaceAll(CHAR_MARKER+"$1"));
 						}
 					}
 				}
