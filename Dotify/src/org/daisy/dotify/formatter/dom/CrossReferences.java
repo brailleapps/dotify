358c2f34b40b75153bd3875ce82e8e7a33d1e8ce
Joel Håkansson <joel.hakansson@mtm.se>
2012-10-04 09:29:44 +0000

    updated translator build documentation

--- Dotify/src/org/daisy/dotify/formatter/dom/CrossReferences.java
+++ Dotify/src/org/daisy/dotify/formatter/dom/CrossReferences.java
@@ -1,14 +1,25 @@
 package org.daisy.dotify.formatter.dom;
 
+/**
+ * Provides an interface for cross references, that is to say
+ * the location of a specific identifier.
+ * @author Joel Håkansson
+ *
+ */
 public interface CrossReferences {
 	
 	/**
-	 * 
-	 * @param refid
+	 * Gets the page for the specified identifier.
+	 * @param refid the identifier to get the page for
 	 * @return returns the page number, one-based
 	 */
 	public Page getPage(String refid);
 	
+	/**
+	 * Gets the volume for the specified identifier.
+	 * @param refid the identifier to get the volume for
+	 * @return returns the volume number, one-based
+	 */
 	public Integer getVolumeNumber(String refid);
 
 }
