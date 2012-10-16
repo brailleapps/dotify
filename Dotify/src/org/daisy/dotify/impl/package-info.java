a5bdc9259b1e5fb88faa69d2ea9f200825d6b5dc
Joel Håkansson <joel.hakansson@mtm.se>
2012-10-16 13:51:09 +0000

    removed empty folders

--- Dotify/src/org/daisy/dotify/impl/package-info.java
+++ Dotify/src/org/daisy/dotify/impl/package-info.java
@@ -1,22 +1,18 @@
 /**
  * <p>
- * Provides implementations for formatter interfaces discovered using the java
+ * Provides implementations for interfaces discovered using the java
  * services API. This includes:
  * </p>
  * <ul>
+ * <li>Config</li>
  * <li>Formatter</li>
+ * <li>Input</li>
  * <li>Paginator</li>
- * <li>VolumeSplitter</li>
  * </ul>
  * <p>
  * The preferred way of instantiating these objects is through the corresponding
- * factory:
+ * factory.
  * </p>
- * <ul>
- * <li>FormatterFactory</li>
- * <li>PaginatorFactory</li>
- * <li>VolumeSplitterFactory</li>
- * </ul>
  * <p>
  * The package may also contain package objects needed to support these
  * implementations. These classes are not part of the API.
