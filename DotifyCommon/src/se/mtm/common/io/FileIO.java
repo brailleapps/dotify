0cb752c1945f2e4e2f3976a73d73ba8b0be46223
Joel Håkansson <joel.hakansson@mtm.se>
2013-09-04 07:28:51 +0000

    Adding a few more functions to dotify-common

--- DotifyCommon/src/se/mtm/common/io/FileIO.java
+++ DotifyCommon/src/se/mtm/common/io/FileIO.java
@@ -1,13 +1,43 @@
 package se.mtm.common.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 import java.util.logging.Logger;
 
 public class FileIO {
+
+	/**
+	 * Copies an input stream to an output stream
+	 * 
+	 * @param is
+	 *            the input stream
+	 * @param os
+	 *            the output stream
+	 * @throws IOException
+	 *             if IO fails
+	 */
+	public static void copy(InputStream is, OutputStream os) throws IOException {
+		InputStream bis = new BufferedInputStream(is);
+		OutputStream bos = new BufferedOutputStream(os);
+		int b;
+		while ((b = bis.read()) != -1) {
+			bos.write(b);
+		}
+		bos.flush();
+		bos.close();
+		bis.close();
+	}
+
+	public static void copy(File input, File output) throws IOException {
+		copy(new FileInputStream(input), new FileOutputStream(output));
+	}
 
 	public static void copyFile(File sourceFile, File destFile) throws IOException {
 		if (!destFile.exists()) {
@@ -28,6 +58,12 @@ public class FileIO {
 				destination.close();
 			}
 		}
+	}
+
+	public static File createTempFile() throws IOException {
+		File ret = File.createTempFile("temp", null, null);
+		ret.deleteOnExit();
+		return ret;
 	}
 
 	public static File createTempDir() throws IOException {
--- DotifyCommon/src/se/mtm/common/xml/XmlTools.java
+++ DotifyCommon/src/se/mtm/common/xml/XmlTools.java
@@ -1,32 +1,71 @@
 package se.mtm.common.xml;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 public class XmlTools {
 
-	public static void transform(File input, File output, InputStream xsltFile, Map<String, Object> params) {
+	public static void transform(File input, File output, InputStream xsltFile, Map<String, Object> params) throws XMLToolsException {
+		StreamSource xslt = new StreamSource(xsltFile);
+
+		Transformer transformer;
 		try {
-			StreamSource xslt = new StreamSource(xsltFile);
-
-			Transformer transformer = TransformerFactory.newInstance().newTransformer(xslt);
-			for (String name : params.keySet()) {
-				transformer.setParameter(name, params.get(name));
-			}
-
-			StreamSource source = new StreamSource(input);
-			StreamResult result = new StreamResult(output);
-
-			transformer.transform(source, result);
-		} catch (Exception e) {
-			e.printStackTrace();
+			transformer = TransformerFactory.newInstance().newTransformer(xslt);
+		} catch (TransformerConfigurationException e) {
+			throw new XMLToolsException(e);
+		} catch (TransformerFactoryConfigurationError e) {
+			throw new XMLToolsException(e);
 		}
+
+		for (String name : params.keySet()) {
+			transformer.setParameter(name, params.get(name));
+		}
+
+		StreamSource source = new StreamSource(input);
+		StreamResult result = new StreamResult(output);
+
+		try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			throw new XMLToolsException(e);
+		}
+	}
+
+	public final static boolean isWellformedXML(File f) throws XMLToolsException {
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = null;
+		try {
+			saxParser = factory.newSAXParser();
+		} catch (ParserConfigurationException e) {
+			throw new XMLToolsException("Failed to set up XML parser.", e);
+		} catch (SAXException e) {
+			throw new XMLToolsException("Failed to set up XML parser.", e);
+		}
+		DefaultHandler dh = new DefaultHandler();
+		try {
+			saxParser.parse(f, dh);
+		} catch (SAXException e) {
+			return false;
+		} catch (IOException e) {
+			throw new XMLToolsException(e);
+		}
+		return true;
 	}
 
 }
