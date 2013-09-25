e3e35ac9710db211f8a0deb6aedcf1ec74931da2
Joel Håkansson <joel.hakansson@mtm.se>
2013-09-25 12:10:24 +0000

    Added normalizer to LayoutEngine Added test of LayoutEngine

--- DotifyFormatter/src/org/daisy/dotify/LayoutEngine.java
+++ DotifyFormatter/src/org/daisy/dotify/LayoutEngine.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.logging.Logger;
 
 import javax.xml.stream.XMLStreamException;
@@ -15,6 +17,7 @@ import org.daisy.dotify.book.BookStruct;
 import org.daisy.dotify.book.Volume;
 import org.daisy.dotify.consumer.translator.BrailleTranslatorFactoryMaker;
 import org.daisy.dotify.obfl.OBFLParserException;
+import org.daisy.dotify.obfl.OBFLWsNormalizer;
 import org.daisy.dotify.obfl.ObflParser;
 import org.daisy.dotify.paginator.Paginator;
 import org.daisy.dotify.paginator.PaginatorFactoryMaker;
@@ -40,6 +43,7 @@ public class LayoutEngine  {
 	private final String mode;
 	private final PagedMediaWriter writer;
 	private final Logger logger;
+	private boolean normalize;
 	
 	/**
 	 * Creates a new instance of LayoutEngineTask.
@@ -54,6 +58,7 @@ public class LayoutEngine  {
 		//this.locale = locale;
 		this.writer = writer;
 		this.logger = Logger.getLogger(LayoutEngine.class.getCanonicalName());
+		this.normalize = true;
 	}
 
 	private BrailleTranslator getTranslator(FilterLocale locale, String mode) {
@@ -64,47 +69,73 @@ public class LayoutEngine  {
 		}
 	}
 
-	public void convert(File input, File output) throws LayoutEngineException {
+	public boolean isNormalizing() {
+		return normalize;
+	}
+
+	public void setNormalizing(boolean normalize) {
+		this.normalize = normalize;
+	}
+
+	public void convert(InputStream input, OutputStream output) throws LayoutEngineException {
+		File f = null;
 		try {
-			logger.info("Parsing input...");
-			//formatterFactory.setLocale(locale);
-			ObflParser obflParser = new ObflParser(locale, mode);
-	        //obflParser.setPaginatorFactory(PaginatorFactory.newInstance());
-			//VolumeSplitterFactory splitterFactory = VolumeSplitterFactory.newInstance();
-			//obflParser.setVolumeSplitterFactory(splitterFactory);
-			obflParser.parse(new FileInputStream(input));
+			if (normalize) {
+				logger.info("Normalizing obfl...");
+				try {
+					f = File.createTempFile("temp", ".tmp");
+					f.deleteOnExit();
+					OBFLWsNormalizer normalizer = new OBFLWsNormalizer(input, new FileOutputStream(f));
+					normalizer.parse();
+					input = new FileInputStream(f);
+				} catch (Exception e) {
+					throw new LayoutEngineException(e);
+				}
+			}
+			try {
 
-			logger.info("Working...");
-			PaginatorFactoryMaker paginatorFactory = PaginatorFactoryMaker.newInstance();
-			Paginator paginator = paginatorFactory.newPaginator();
-			paginator.open(translator, obflParser.getBlockStruct().getBlockSequenceIterable());
-			
-			BookStruct bookStruct = new BookStruct(
-					paginator,
-					obflParser.getVolumeContentFormatter(),
-					translator,
-					paginatorFactory
-				);
-			Iterable<Volume> volumes = bookStruct.getVolumes();
+				logger.info("Parsing input...");
+				// formatterFactory.setLocale(locale);
+				ObflParser obflParser = new ObflParser(locale, mode);
+				// obflParser.setPaginatorFactory(PaginatorFactory.newInstance());
+				// VolumeSplitterFactory splitterFactory =
+				// VolumeSplitterFactory.newInstance();
+				// obflParser.setVolumeSplitterFactory(splitterFactory);
+				obflParser.parse(input);
 
-			logger.info("Rendering output...");
-			writer.open(new FileOutputStream(output), obflParser.getMetaData());
-			//splitterFactory.newSplitter().split(bookStruct)
+				logger.info("Working...");
+				PaginatorFactoryMaker paginatorFactory = PaginatorFactoryMaker.newInstance();
+				Paginator paginator = paginatorFactory.newPaginator();
+				paginator.open(translator, obflParser.getBlockStruct().getBlockSequenceIterable());
 
-			WriterHandler wh = new WriterHandler();
-			wh.write(volumes, writer);
-			writer.close();
+				BookStruct bookStruct = new BookStruct(paginator, obflParser.getVolumeContentFormatter(), translator, paginatorFactory);
+				Iterable<Volume> volumes = bookStruct.getVolumes();
 
-		} catch (FileNotFoundException e) {
-			throw new LayoutEngineException("FileNotFoundException while running task. ", e);
-		} catch (IOException e) {
-			throw new LayoutEngineException("IOException while running task. ", e);
-		} catch (PagedMediaWriterException e) {
-			throw new LayoutEngineException("Could not open media writer.", e);
-		} catch (XMLStreamException e) {
-			throw new LayoutEngineException("XMLStreamException while running task.", e);
-		} catch (OBFLParserException e) {
-			throw new LayoutEngineException("FormatterException while running task.", e);
+				logger.info("Rendering output...");
+				writer.open(output, obflParser.getMetaData());
+				// splitterFactory.newSplitter().split(bookStruct)
+
+				WriterHandler wh = new WriterHandler();
+				wh.write(volumes, writer);
+				writer.close();
+
+			} catch (FileNotFoundException e) {
+				throw new LayoutEngineException("FileNotFoundException while running task. ", e);
+			} catch (IOException e) {
+				throw new LayoutEngineException("IOException while running task. ", e);
+			} catch (PagedMediaWriterException e) {
+				throw new LayoutEngineException("Could not open media writer.", e);
+			} catch (XMLStreamException e) {
+				throw new LayoutEngineException("XMLStreamException while running task.", e);
+			} catch (OBFLParserException e) {
+				throw new LayoutEngineException("FormatterException while running task.", e);
+			}
+		} finally {
+			if (f != null) {
+				if (!f.delete()) {
+					f.deleteOnExit();
+				}
+			}
 		}
 	}
 
---
+++ DotifyFormatter/test/org/daisy/dotify/LayoutEngineTest.java
@@ -0,0 +1,61 @@
+package org.daisy.dotify;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.daisy.dotify.api.translator.BrailleTranslatorFactory;
+import org.daisy.dotify.text.FilterLocale;
+import org.daisy.dotify.writer.TextMediaWriter;
+import org.junit.Test;
+public class LayoutEngineTest {
+	
+	@Test
+	public void testLayoutEnginge() throws LayoutEngineException, IOException {
+		LayoutEngine engine = new LayoutEngine(FilterLocale.parse("en"),
+				BrailleTranslatorFactory.MODE_BYPASS,
+				new TextMediaWriter("utf-8"));
+		File res = File.createTempFile("TestResult", ".tmp");
+
+		engine.convert(this.getClass().getResourceAsStream("resource-files/obfl-input.obfl"), new FileOutputStream(res));
+		
+		try {
+			int ret = compareBinary(this.getClass().getResourceAsStream("resource-files/obfl-expected.txt"), new FileInputStream(res));
+			assertEquals("Binary compare is equal", -1, ret);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		} finally {
+			if (!res.delete()) {
+				res.deleteOnExit();
+			}
+		}
+	}
+
+	public int compareBinary(InputStream f1, InputStream f2) throws IOException {
+		InputStream bf1 = new BufferedInputStream(f1);
+		InputStream bf2 = new BufferedInputStream(f2);
+		int pos = 0;
+		try {
+			int b1;
+			int b2;
+			while ((b1 = bf1.read()) != -1 & b1 == (b2 = bf2.read())) {
+				pos++;
+				// continue
+			}
+			if (b1 != -1 || b2 != -1) {
+				return pos;
+			}
+			return -1;
+		} finally {
+			bf1.close();
+			bf2.close();
+		}
+	}
+}
