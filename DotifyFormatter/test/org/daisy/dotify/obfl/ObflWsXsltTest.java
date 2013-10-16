a7a09681757308cb4fb4a18d7003489cac98a62b
Joel Håkansson <joel.hakansson@mtm.se>
2013-10-16 07:32:17 +0000

    separated whitespace test into several smaller ones (for clarity) added a test for no-break space

--- DotifyFormatter/test/org/daisy/dotify/obfl/ObflWsXsltTest.java
+++ DotifyFormatter/test/org/daisy/dotify/obfl/ObflWsXsltTest.java
@@ -17,15 +17,81 @@ import org.junit.Test;
 public class ObflWsXsltTest {
 
 	@Test
-	public void testWsNormalizer() throws IOException, XMLStreamException {
+	public void testWsNormalizer_01() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-01.xml", "resource-files/ws-test-expected-01.xml");
+		assertTrue("Compare (Toc) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_02() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-02.xml", "resource-files/ws-test-expected-02.xml");
+		assertTrue("Compare (Block) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_03() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-03.xml", "resource-files/ws-test-expected-03.xml");
+		assertTrue("Compare (Span) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_04() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-04.xml", "resource-files/ws-test-expected-04.xml");
+		assertTrue("Compare (Line breaks) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_05() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-05.xml", "resource-files/ws-test-expected-05.xml");
+		assertTrue("Compare (Leader) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_06() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-06.xml", "resource-files/ws-test-expected-06.xml");
+		assertTrue("Compare (Evaluate) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_07() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-07.xml", "resource-files/ws-test-expected-07.xml");
+		assertTrue("Compare (Page number) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_08() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-08.xml", "resource-files/ws-test-expected-08.xml");
+		assertTrue("Compare (Marker) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_09() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-09.xml", "resource-files/ws-test-expected-09.xml");
+		assertTrue("Compare (Anchor) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_10() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-10.xml", "resource-files/ws-test-expected-10.xml");
+		assertTrue("Compare (Style) failed at byte: " + ret, ret == -1);
+	}
+
+	@Test
+	public void testWsNormalizer_11() throws IOException, XMLStreamException {
+		int ret = testWsNormalizer("resource-files/ws-test-input-11.xml", "resource-files/ws-test-expected-11.xml");
+		assertTrue("Compare (NBSP) failed at byte: " + ret, ret == -1);
+	}
+
+	// Helpers
+	public int testWsNormalizer(String input, String expected) throws IOException, XMLStreamException {
 		
 		File in = File.createTempFile("TestInput", ".tmp");
-		copy(this.getClass().getResourceAsStream("resource-files/ws-test-input.xml"), new FileOutputStream(in));
+		copy(this.getClass().getResourceAsStream(input), new FileOutputStream(in));
 		
 		File normalizedFile = File.createTempFile("TestResult", ".tmp");
 		OBFLWsNormalizer t = new OBFLWsNormalizer(new FileInputStream(in), new FileOutputStream(normalizedFile));
 		t.parse();
-		int ret = compareBinary(new FileInputStream(normalizedFile), this.getClass().getResourceAsStream("resource-files/ws-test-expected.xml"));
+		int ret = compareBinary(new FileInputStream(normalizedFile), this.getClass().getResourceAsStream(expected));
 		
 		if (!normalizedFile.delete()) {
 			normalizedFile.deleteOnExit();
@@ -34,10 +100,9 @@ public class ObflWsXsltTest {
 			in.deleteOnExit();
 		}
 		
-		assertTrue("Compare failed at byte: " + ret, ret==-1);
+		return ret;
 	}
-	
-	//Helpers
+
 	public static void copy(InputStream is, OutputStream os) throws IOException {
 		InputStream bis = new BufferedInputStream(is);
 		OutputStream bos = new BufferedOutputStream(os);
