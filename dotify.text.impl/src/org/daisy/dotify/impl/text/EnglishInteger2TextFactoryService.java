6b2eea0351c4be82a2b1057afbc15bb1c607ed01
Joel Håkansson <joel.hakansson@mtm.se>
2013-11-08 10:10:47 +0000

    Added support for listing locales in Integer2TextFactory (and related)

--- dotify.text.impl/src/org/daisy/dotify/impl/text/EnglishInteger2TextFactoryService.java
+++ dotify.text.impl/src/org/daisy/dotify/impl/text/EnglishInteger2TextFactoryService.java
@@ -1,5 +1,9 @@
 package org.daisy.dotify.impl.text;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.daisy.dotify.api.text.Integer2TextFactory;
 import org.daisy.dotify.api.text.Integer2TextFactoryService;
 
@@ -8,14 +12,28 @@ import aQute.bnd.annotation.component.Component;
 @Component
 public class EnglishInteger2TextFactoryService implements
 		Integer2TextFactoryService {
+	
+	private final static List<String> locales;
+	static {
+		locales = new ArrayList<String>();
+		locales.add("en");
+	}
 
 	public boolean supportsLocale(String locale) {
-		return "en".equals(locale);
+		for (String l : locales) {
+			if (l.equalsIgnoreCase(locale)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public Integer2TextFactory newFactory() {
 		return new EnglishInteger2TextFactory();
 	}
 
+	public Collection<String> listLocales() {
+		return locales;
+	}
 
 }
--- dotify.text.impl/src/org/daisy/dotify/impl/text/SwedishInteger2TextFactoryService.java
+++ dotify.text.impl/src/org/daisy/dotify/impl/text/SwedishInteger2TextFactoryService.java
@@ -1,5 +1,9 @@
 package org.daisy.dotify.impl.text;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.daisy.dotify.api.text.Integer2TextFactory;
 import org.daisy.dotify.api.text.Integer2TextFactoryService;
 
@@ -8,14 +12,30 @@ import aQute.bnd.annotation.component.Component;
 @Component
 public class SwedishInteger2TextFactoryService implements
 		Integer2TextFactoryService {
+	
+	private final static List<String> locales;
+	static {
+		locales = new ArrayList<String>();
+		locales.add("sv-SE");
+		locales.add("sv");
+	}
+	
 
 	public boolean supportsLocale(String locale) {
-		return "sv-SE".equalsIgnoreCase(locale);
+		for (String l : locales) {
+			if (l.equalsIgnoreCase(locale)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public Integer2TextFactory newFactory() {
 		return new SwedishInteger2TextFactory();
 	}
 
+	public Collection<String> listLocales() {
+		return locales;
+	}
 
 }
