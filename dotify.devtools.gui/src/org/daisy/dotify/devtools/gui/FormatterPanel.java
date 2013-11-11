bf2e117b3edf5b820ab525944f45af1cee5cea96
Joel Håkansson <joel.hakansson@mtm.se>
2013-11-11 10:19:19 +0000

    implemented formatter support in OSGi dev. gui

--- dotify.devtools.gui/src/org/daisy/dotify/devtools/gui/FormatterPanel.java
+++ dotify.devtools.gui/src/org/daisy/dotify/devtools/gui/FormatterPanel.java
@@ -3,14 +3,27 @@ package org.daisy.dotify.devtools.gui;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-import org.daisy.dotify.api.engine.FormatterEngineFactoryMakerService;
+import org.daisy.dotify.api.engine.FormatterEngine;
+import org.daisy.dotify.api.engine.FormatterEngineFactoryService;
+import org.daisy.dotify.api.engine.LayoutEngineException;
+import org.daisy.dotify.api.translator.BrailleTranslatorFactory;
+import org.daisy.dotify.writer.PEFMediaWriter;
 import org.osgi.framework.BundleContext;
 
 public class FormatterPanel extends MyPanel {
@@ -18,32 +31,44 @@ public class FormatterPanel extends MyPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -8051107255963928066L;
-	private final JTextField textField;
+	private final JButton chooseFile;
+	private final JFileChooser chooser;
 	private final JTextArea outputField;
+	private File input;
 
 	private FormatterTracker tracker;
 	
 	public FormatterPanel() {
 		super(new GridLayout(2, 1));
+		
+		input = null;
 
 		Font f = new Font(null, Font.BOLD, 26);
 
 		outputField = new JTextArea();
 		outputField.setFont(f);
 		outputField.setEditable(false);
+		
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	            "OBFL-files", "obfl");
 
-		textField = new JTextField();
-		textField.setFont(f);
+		chooser = new JFileChooser();
+		chooser.setFileFilter(filter);
+		
+		chooseFile = new JButton("Choose file...");
+		chooseFile.addActionListener(new ActionListener() {
 
-		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int returnVal =chooser.showOpenDialog(null);
+			    if(returnVal == JFileChooser.APPROVE_OPTION) {
+			       input = chooser.getSelectedFile();
+			       chooseFile.setText(chooser.getSelectedFile().getName());
+			       updateResult();
+			    }
+			}});
 
-			public void keyReleased(KeyEvent e) {
-				updateResult();
-			}
-
-		});
-
-		add(textField);
+		add(chooseFile);
 		add(new JScrollPane(outputField));
 
 		setPreferredSize(new Dimension(500, 400));
@@ -54,23 +79,28 @@ public class FormatterPanel extends MyPanel {
 			outputField.setText("No locale selected");
 		} else {
 
-				FormatterEngineFactoryMakerService t = tracker.get();
+				FormatterEngineFactoryService t = tracker.get();
 				if (t == null) {
-					outputField.setText("No conversion");
+					outputField.setText("No formatter detected");
 				} else {
-					if (textField.getText().equals("")) {
-						outputField.setText("");
+					if (input==null) {
+						outputField.setText("No file selected.");
+						chooseFile.setText("Choose file...");
+					} else if (!input.isFile()) { 
+						outputField.setText("File doesn't exist " + input);
 					} else {
-							//try {
-								outputField.setText("not implemented");
-							//} catch (Integer2TextConfigurationException e1) {
-								outputField.setText("Locale not supported: " + getTargetLocale() + "\n");
-								/*
-								outputField.append("Supported values:\n");
-								for (String s : t.listLocales()) {
-									outputField.append(s+"\n");
-								}*/
-							//}
+						Properties p = new Properties();
+						p.put("date", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+						FormatterEngine e = t.newFormatterEngine(getTargetLocale(), BrailleTranslatorFactory.MODE_UNCONTRACTED, new PEFMediaWriter(p));
+						File out = new File(input.getParentFile(), input.getName()+".pef");
+						try {
+							e.convert(new FileInputStream(input), new FileOutputStream(out));
+							outputField.setText("Done! " + out);
+						} catch (FileNotFoundException e1) {
+							outputField.setText(e1.toString());
+						} catch (LayoutEngineException e1) {
+							outputField.setText("Not supported");
+						}
 					}
 				}
 
--- dotify.devtools.gui/src/org/daisy/dotify/devtools/gui/FormatterTracker.java
+++ dotify.devtools.gui/src/org/daisy/dotify/devtools/gui/FormatterTracker.java
@@ -1,12 +1,12 @@
 package org.daisy.dotify.devtools.gui;
 
-import org.daisy.dotify.api.engine.FormatterEngineFactoryMakerService;
+import org.daisy.dotify.api.engine.FormatterEngineFactoryService;
 import org.osgi.framework.BundleContext;
 
-public class FormatterTracker extends MyTracker<FormatterEngineFactoryMakerService> {
+public class FormatterTracker extends MyTracker<FormatterEngineFactoryService> {
 
 	public FormatterTracker(BundleContext context) {
-		super(context, FormatterEngineFactoryMakerService.class.getName());
+		super(context, FormatterEngineFactoryService.class.getName());
 	}
 
 }
