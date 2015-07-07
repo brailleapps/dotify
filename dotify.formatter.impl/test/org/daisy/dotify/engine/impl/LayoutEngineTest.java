620fb8efd9ec0c4f376c4f8a94568067f7be346e
Joel Håkansson <joel.hakansson@mtm.se>
2015-07-07 14:47:13 +0200

    Moves formatter tests to dotify.formatter.impl

--- dotify.formatter.impl/test/org/daisy/dotify/engine/impl/LayoutEngineTest.java
+++ dotify.formatter.impl/test/org/daisy/dotify/engine/impl/LayoutEngineTest.java
@@ -1,4 +1,4 @@
-package org.daisy.dotify;
+package org.daisy.dotify.engine.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -10,10 +10,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.xml.transform.stream.StreamSource;
-
-import org.daisy.braille.pef.PEFFileCompare;
-import org.daisy.braille.pef.PEFFileCompareException;
 import org.daisy.dotify.api.engine.FormatterEngine;
 import org.daisy.dotify.api.engine.LayoutEngineException;
 import org.daisy.dotify.api.translator.BrailleTranslatorFactory;
@@ -23,7 +19,7 @@ import org.daisy.dotify.api.writer.PagedMediaWriterFactory;
 import org.daisy.dotify.consumer.engine.FormatterEngineMaker;
 import org.daisy.dotify.consumer.writer.PagedMediaWriterFactoryMaker;
 import org.junit.Test;
-public class LayoutEngineTest {
+public class LayoutEngineTest extends AbstractFormatterEngineTest {
 	
 	@Test
 	public void testLayoutEnginge() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
@@ -51,72 +47,42 @@ public class LayoutEngineTest {
 
 	@Test
 	public void testLayoutEngingeDLS() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-dls.obfl", "resource-files/obfl-dls-expected.pef");
+		testPEF("resource-files/obfl-input-dls.obfl", "resource-files/obfl-dls-expected.pef", true);
 	}
 	
 	@Test
 	public void testLayoutEngingeBorder() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-border.obfl", "resource-files/obfl-border-expected.pef");
+		testPEF("resource-files/obfl-input-border.obfl", "resource-files/obfl-border-expected.pef", false);
 	}
 	
 	@Test
 	public void testLayoutEngingeToc() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-toc.obfl", "resource-files/obfl-toc-expected.pef");
+		testPEF("resource-files/obfl-input-toc.obfl", "resource-files/obfl-toc-expected.pef", false);
 	}
 	
 	@Test
 	public void testLayoutEngingeContentItems() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-content-items.obfl", "resource-files/obfl-content-items-expected.pef");
+		testPEF("resource-files/obfl-input-content-items.obfl", "resource-files/obfl-content-items-expected.pef", false);
 	}
 	
 	@Test
 	public void testLayoutEngingeContentItemsFallback() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-content-items-fallback.obfl", "resource-files/obfl-content-items-fallback-expected.pef");
+		testPEF("resource-files/obfl-input-content-items-fallback.obfl", "resource-files/obfl-content-items-fallback-expected.pef", false);
 	}
 	
 	@Test
 	public void testLayoutEngingeContentItemsFallback2() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-content-items-fallback2.obfl", "resource-files/obfl-content-items-fallback2-expected.pef");
+		testPEF("resource-files/obfl-input-content-items-fallback2.obfl", "resource-files/obfl-content-items-fallback2-expected.pef", false);
 	}
 	
 	@Test
 	public void testLayoutEngingeContentItemsFallback3() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-content-items-fallback3.obfl", "resource-files/obfl-content-items-fallback3-expected.pef");
+		testPEF("resource-files/obfl-input-content-items-fallback3.obfl", "resource-files/obfl-content-items-fallback3-expected.pef", false);
 	}
 	
 	@Test (expected = RuntimeException.class)
 	public void testLayoutEngineContentItemsNoFallback() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		testPEF("resource-files/obfl-input-content-items-no-fallback.obfl", null);
-	}
-
-	private void testPEF(String input, String expected) throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
-		FormatterEngine engine = FormatterEngineMaker.newInstance().newFormatterEngine("sv-SE",
-				BrailleTranslatorFactory.MODE_UNCONTRACTED, 
-				PagedMediaWriterFactoryMaker.newInstance().newPagedMediaWriter(MediaTypes.PEF_MEDIA_TYPE));
-
-		File res = File.createTempFile("TestResult", ".tmp");
-		res.deleteOnExit();
-
-		engine.convert(this.getClass().getResourceAsStream(input), new FileOutputStream(res));
-
-		try {
-			PEFFileCompare cmp = new PEFFileCompare();
-			cmp.compare(new StreamSource(this.getClass().getResourceAsStream(expected)), new StreamSource(new FileInputStream(res)));
-			assertEquals("Binary compare is equal", -1, cmp.getPos());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		} catch (PEFFileCompareException e) {
-			e.printStackTrace();
-			fail();
-		} finally {
-			if (!res.delete()) {
-				System.err.println("Delete failed.");
-			}
-			if (res.isFile()) {
-				System.out.println(res.getAbsolutePath());
-			}
-		}
+		testPEF("resource-files/obfl-input-content-items-no-fallback.obfl", null, false);
 	}
 
 	public int compareBinary(InputStream f1, InputStream f2) throws IOException {
