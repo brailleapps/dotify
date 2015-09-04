8279df57c587b8da4126bd972a41247e7319256a
Joel Håkansson <joel.hakansson@mtm.se>
2015-09-04 12:05:41 +0200

    Adds an integration test that runs a conversion

--- Dotify/integrationtest/base/TaskSystemFactoryMakerTestbase.java
+++ Dotify/integrationtest/base/TaskSystemFactoryMakerTestbase.java
@@ -4,15 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 import org.daisy.dotify.SystemKeys;
 import org.daisy.dotify.api.tasks.InternalTask;
+import org.daisy.dotify.api.tasks.ReadOnlyTask;
+import org.daisy.dotify.api.tasks.ReadWriteTask;
 import org.daisy.dotify.api.tasks.TaskSystem;
 import org.daisy.dotify.api.tasks.TaskSystemException;
 import org.daisy.dotify.api.tasks.TaskSystemFactoryException;
 import org.daisy.dotify.api.tasks.TaskSystemFactoryMakerService;
+import org.daisy.dotify.common.io.TempFileHandler;
 import org.junit.Test;
 
 public abstract class TaskSystemFactoryMakerTestbase {
@@ -46,7 +50,7 @@ public abstract class TaskSystemFactoryMakerTestbase {
 		HashMap<String, Object> options = new HashMap<String, Object>();
 		//This test really shows how this code has to improve...
 		File f = new File("integrationtest/base/resource-files/dtbook.xml");
-		options.put(SystemKeys.INPUT_FORMAT, "dtbook");
+		options.put(SystemKeys.INPUT_FORMAT, "xml");
 		options.put(SystemKeys.INPUT, f.getAbsolutePath());
 		List<InternalTask> tasks = tf.compile(options);
 		
@@ -55,6 +59,40 @@ public abstract class TaskSystemFactoryMakerTestbase {
 		assertEquals("Conformance checker: validation-files/dtbook.sch", tasks.get(0).getName());
 		assertEquals("Input to OBFL converter: xslt-files/dtbook2flow_sv_SE_braille.xsl", tasks.get(1).getName());
 		assertEquals("OBFL to PEF converter", tasks.get(2).getName());
+	}
+	
+	@Test
+	public void runFactoryForSwedish() throws TaskSystemFactoryException, TaskSystemException, IOException {
+		//Setup
+		TaskSystemFactoryMakerService factory = getTaskSystemFMS();
+		TaskSystem tf = factory.newTaskSystem("sv-SE", "obfl");
+		assertNotNull(tf);
+		HashMap<String, Object> options = new HashMap<String, Object>();
+
+		//This test really shows how this code has to improve...
+		File f = new File("integrationtest/base/resource-files/dtbook.xml");
+		options.put(SystemKeys.INPUT_FORMAT, "xml");
+		options.put(SystemKeys.INPUT, f.getAbsolutePath());
+		List<InternalTask> tasks = tf.compile(options);
+
+		File out = File.createTempFile(this.getClass().getName(), ".tmp");
+		try {
+			TempFileHandler fj = new TempFileHandler(f, out);
+			for (InternalTask task : tasks) {
+				if (task instanceof ReadWriteTask) {
+					((ReadWriteTask)task).execute(fj.getInput(), fj.getOutput());
+					fj.reset();
+				} else if (task instanceof ReadOnlyTask) {
+					((ReadOnlyTask)task).execute(fj.getInput());
+				} else {
+				}
+			}
+			fj.close();
+		} finally {
+			if (!out.delete()) {
+				out.deleteOnExit();
+			}
+		}
 	}
 	
 	@Test
