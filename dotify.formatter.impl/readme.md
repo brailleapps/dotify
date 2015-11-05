30c8000bbf96facbf9c8b1759af4f5e66e360d19
Bert Frees <bertfrees@gmail.com>
2015-11-05 17:33:29 +0100

    Moving/copying tests from pipeline-mod-braille to dotify
    The tests are automatically converted from XProcSpec files. They
    currently have no useful title/description yet. Probably a selection
    needs to be done because part of the tests may already be covered and
    part of the tests are generated from test_format.xprocspec which is
    mainly concerned about the css-to-obfl conversion, less about the
    obfl-to-pef conversion.
    
    Currently the test fails and I'm having trouble finding out what goes
    wrong. Need help.
    
    see also https://github.com/joeha480/dotify/issues/114

--- dotify.formatter.impl/readme.md
+++ dotify.formatter.impl/readme.md
@@ -2,3 +2,22 @@
 dotify.formatter.impl contains an implementation of the formatter interfaces of [dotify.api](https://github.com/joeha480/dotify/tree/master/dotify.api).
 
 [Here](https://rawgit.com/joeha480/dotify/master/dotify.formatter.impl/test/org/daisy/dotify/engine/impl/resource-files/tests-overview.xml) are a few OBFL examples.
+
+## How to add tests to dotify.formatter.impl
+
+OBFL-to-PEF tests can be added by including lines such as the
+following:
+
+```java
+testPEF("resource-files/foo-input.obfl", "resource-files/foo-expected.pef", false);
+```
+
+in a class that extends `AbstractFormatterEngineTest`. Tests are run
+with `gradle test`.
+
+If you want the tests to be included in the overview page at
+`test/org/daisy/dotify/engine/impl/resource-files/tests-overview.xml`,
+the OBFL and PEF files need to be placed in
+`test/org/daisy/dotify/engine/impl/resource-files` and named according
+to the pattern `foo-input.obfl`/`foo-expected.pef`. The page can be
+updated with `gradle updateTestsOverviewFile`.
---
+++ dotify.formatter.impl/test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
@@ -0,0 +1,79 @@
+package org.daisy.dotify.engine.impl;
+
+import java.io.IOException;
+
+import org.daisy.dotify.api.engine.LayoutEngineException;
+import org.daisy.dotify.api.writer.PagedMediaWriterConfigurationException;
+import org.junit.Test;
+
+public class TakenFromDP2Test extends AbstractFormatterEngineTest {
+	
+	@Test
+	public void test() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_01-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_01-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_02-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_02-expected.pef", false);
+		//testPEF("resource-files/test_obfl-to-pef.xprocspec/test_03-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_03-expected.pef", false);
+		//testPEF("resource-files/test_obfl-to-pef.xprocspec/test_04-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_04-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_05-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_05-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_06-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_06-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_07-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_07-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_08-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_08-expected.pef", false);
+		//testPEF("resource-files/test_obfl-to-pef.xprocspec/test_09-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_09-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_10-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_10-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_11-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_11-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_12-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_12-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_13-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_13-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_14-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_14-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_15-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_15-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_16-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_16-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_17-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_17-expected.pef", false);
+		//testPEF("resource-files/test_obfl-to-pef.xprocspec/test_18-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_18-expected.pef", false);
+		testPEF("resource-files/test_obfl-to-pef.xprocspec/test_19-input.obfl", "resource-files/test_obfl-to-pef.xprocspectest_19-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_01-input.obfl", "resource-files/test_format.xprocspec/test_01-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_02-input.obfl", "resource-files/test_format.xprocspec/test_02-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_03-input.obfl", "resource-files/test_format.xprocspec/test_03-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_04-input.obfl", "resource-files/test_format.xprocspec/test_04-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_05-input.obfl", "resource-files/test_format.xprocspec/test_05-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_06-input.obfl", "resource-files/test_format.xprocspec/test_06-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_07-input.obfl", "resource-files/test_format.xprocspec/test_07-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_08-input.obfl", "resource-files/test_format.xprocspec/test_08-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_09-input.obfl", "resource-files/test_format.xprocspec/test_09-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_10-input.obfl", "resource-files/test_format.xprocspec/test_10-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_11-input.obfl", "resource-files/test_format.xprocspec/test_11-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_12-input.obfl", "resource-files/test_format.xprocspec/test_12-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_13-input.obfl", "resource-files/test_format.xprocspec/test_13-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_14-input.obfl", "resource-files/test_format.xprocspec/test_14-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_15-input.obfl", "resource-files/test_format.xprocspec/test_15-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_16-input.obfl", "resource-files/test_format.xprocspec/test_16-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_17-input.obfl", "resource-files/test_format.xprocspec/test_17-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_18-input.obfl", "resource-files/test_format.xprocspec/test_18-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_19-input.obfl", "resource-files/test_format.xprocspec/test_19-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_20-input.obfl", "resource-files/test_format.xprocspec/test_20-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_21-input.obfl", "resource-files/test_format.xprocspec/test_21-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_22-input.obfl", "resource-files/test_format.xprocspec/test_22-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_23-input.obfl", "resource-files/test_format.xprocspec/test_23-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_24-input.obfl", "resource-files/test_format.xprocspec/test_24-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_25-input.obfl", "resource-files/test_format.xprocspec/test_25-expected.pef", false);
+		//testPEF("resource-files/test_format.xprocspec/test_26-input.obfl", "resource-files/test_format.xprocspec/test_26-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_27-input.obfl", "resource-files/test_format.xprocspec/test_27-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_28-input.obfl", "resource-files/test_format.xprocspec/test_28-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_29-input.obfl", "resource-files/test_format.xprocspec/test_29-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_30-input.obfl", "resource-files/test_format.xprocspec/test_30-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_31-input.obfl", "resource-files/test_format.xprocspec/test_31-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_32-input.obfl", "resource-files/test_format.xprocspec/test_32-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_33-input.obfl", "resource-files/test_format.xprocspec/test_33-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_34-input.obfl", "resource-files/test_format.xprocspec/test_34-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_35-input.obfl", "resource-files/test_format.xprocspec/test_35-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_36-input.obfl", "resource-files/test_format.xprocspec/test_36-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_37-input.obfl", "resource-files/test_format.xprocspec/test_37-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_38-input.obfl", "resource-files/test_format.xprocspec/test_38-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_39-input.obfl", "resource-files/test_format.xprocspec/test_39-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_40-input.obfl", "resource-files/test_format.xprocspec/test_40-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_41-input.obfl", "resource-files/test_format.xprocspec/test_41-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_42-input.obfl", "resource-files/test_format.xprocspec/test_42-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_43-input.obfl", "resource-files/test_format.xprocspec/test_43-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_44-input.obfl", "resource-files/test_format.xprocspec/test_44-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_45-input.obfl", "resource-files/test_format.xprocspec/test_45-expected.pef", false);
+		testPEF("resource-files/test_format.xprocspec/test_46-input.obfl", "resource-files/test_format.xprocspec/test_46-expected.pef", false);
+	}
+}
