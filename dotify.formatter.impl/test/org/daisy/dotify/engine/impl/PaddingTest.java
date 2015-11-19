28606b0b88454864f1470ae5dbae0187d9decf26
Joel Håkansson <joel.hakansson@mtm.se>
2015-11-19 14:41:30 +0100

    Adds tests for when padding height is greater than page height

--- dotify.formatter.impl/test/org/daisy/dotify/engine/impl/PaddingTest.java
+++ dotify.formatter.impl/test/org/daisy/dotify/engine/impl/PaddingTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.daisy.dotify.api.engine.LayoutEngineException;
 import org.daisy.dotify.api.writer.PagedMediaWriterConfigurationException;
+import org.junit.Ignore;
 import org.junit.Test;
 public class PaddingTest extends AbstractFormatterEngineTest {
 	
@@ -20,6 +21,12 @@ public class PaddingTest extends AbstractFormatterEngineTest {
 	@Test
 	public void testPadding_03() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/padding-nested-input.obfl", "resource-files/padding-nested-expected.pef", false);
+	}
+	
+	@Test
+	@Ignore ("Fails with an IndexOutOfBoundsException. A more descriptive error would be good, see issue #141")
+	public void testPadding_04() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
+		testPEF("resource-files/padding-height-input.obfl", "resource-files/padding-height-expected.pef", false);
 	}
 
 }
