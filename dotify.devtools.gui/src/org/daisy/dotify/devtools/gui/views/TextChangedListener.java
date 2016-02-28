3243b1b6e751ab429fbbad13c69a2d5c47eac8a6
Joel Håkansson <joel.hakansson@mtm.se>
2016-02-28 10:09:31 +0100

    Moves dotify.devtools.gui into a separate project

--- dotify.devtools.gui/src/org/daisy/dotify/devtools/gui/views/TextChangedListener.java
+++
@@ -1,8 +0,0 @@
-package org.daisy.dotify.devtools.gui.views;
-
-import java.util.EventListener;
-
-public interface TextChangedListener extends EventListener {
-
-	public void textChanged(TextChangedEvent event);
-}
