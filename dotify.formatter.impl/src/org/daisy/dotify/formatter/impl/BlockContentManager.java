brailleapps/dotify

commit 921d38f7e8879d48c38afe7759c19fe912fcf928

Fix issue with leader followed by more than one segment
see https://github.com/joeha480/obfl/issues/31

dotify.formatter.impl/src/org/daisy/dotify/formatter/impl/BlockContentManager.java
@@ -1,6 +1,8 @@
 package org.daisy.dotify.formatter.impl;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Stack;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
@@ -12,6 +14,7 @@ import org.daisy.dotify.api.formatter.Marker;
 import org.daisy.dotify.api.translator.BrailleTranslatorResult;
 import org.daisy.dotify.api.translator.Translatable;
 import org.daisy.dotify.api.translator.TranslationException;
+import org.daisy.dotify.api.translator.UnsupportedMetricException;
 import org.daisy.dotify.common.text.StringTools;
 
 /**
@@ -75,7 +78,7 @@ class BlockContentManager extends AbstractBlockContentManager {
 				case NewLine:
 				{
 					//flush
-					layout("", null, null);
+					layoutLeader();
 					MarginProperties ret = new MarginProperties(leftMargin.getContent()+StringTools.fill(fcontext.getSpaceCharacter(), rdp.getTextIndent()), leftMargin.isSpaceOnly());
 					rows.add(createAndConfigureEmptyNewRow(ret));
 					break;
@@ -83,7 +86,7 @@ class BlockContentManager extends AbstractBlockContentManager {
 				case Text:
 				{
 					TextSegment ts = (TextSegment)s;
-					layout(
+					layoutAfterLeader(
 							Translatable.text(ts.getText()).
 							locale(ts.getTextProperties().getLocale()).
 							hyphenate(ts.getTextProperties().isHyphenating()).build(),
@@ -93,7 +96,7 @@ class BlockContentManager extends AbstractBlockContentManager {
 				case Leader:
 				{
 					if (currentLeader!=null) {
-						layout("", null, null);
+						layoutLeader();
 					}
 					currentLeader= (Leader)s;
 					break;
@@ -108,9 +111,9 @@ class BlockContentManager extends AbstractBlockContentManager {
 					}
 					//TODO: translate references using custom language?
 					if (page==null) {
-						layout("??",  null, null);
+						layoutAfterLeader(Translatable.text("??").locale(null).build(), null);
 					} else {
-						layout("" + rs.getNumeralStyle().format(page), null, null);
+						layoutAfterLeader(Translatable.text("" + rs.getNumeralStyle().format(page)).locale(null).build(), null);
 					}
 					break;
 				}
@@ -118,7 +121,8 @@ class BlockContentManager extends AbstractBlockContentManager {
 				{
 					isVolatile = true;
 					Evaluate e = (Evaluate)s;
-					layout(Translatable.text(e.getExpression().render(context)).
+					layoutAfterLeader(
+							Translatable.text(e.getExpression().render(context)).
 							locale(e.getTextProperties().getLocale()).
 							hyphenate(e.getTextProperties().isHyphenating()).
 							build(), 
@@ -149,7 +153,7 @@ class BlockContentManager extends AbstractBlockContentManager {
 		}
 		
 		if (currentLeader!=null || item!=null) {
-			layout("",  null, null);
+			layoutLeader();
 		}
 		if (rows.size()>0) {
 			rows.get(0).addAnchors(0, groupAnchors);
@@ -159,6 +163,37 @@ class BlockContentManager extends AbstractBlockContentManager {
 		}
 	}
 	
+	private List<BrailleTranslatorResult> layoutAfterLeader = null;
+	private String currentLeaderMode = null;
+	
+	private void layoutAfterLeader(Translatable spec, String mode) {
+		if (currentLeader!=null) {
+			if (layoutAfterLeader == null) {
+				layoutAfterLeader = new ArrayList<BrailleTranslatorResult>();
+				// use the mode of the first following segment to translate the leader pattern
+				currentLeaderMode = mode;
+			}
+			try {
+				layoutAfterLeader.add(fcontext.getTranslator(mode).translate(spec));
+			} catch (TranslationException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			layout(spec, mode);
+		}
+	}
+	
+	private void layoutLeader() {
+		if (currentLeader!=null) {
+			// layout() sets currentLeader to null
+			if (layoutAfterLeader == null) {
+				layout("", null, null);
+			} else {
+				layout(new AggregatedBrailleTranslatorResult(layoutAfterLeader), currentLeaderMode);
+			}
+		}
+	}
+
 	/**
 	 * 
 	 * @param margin rdp.getSpaceBefore()
@@ -340,5 +375,78 @@ class BlockContentManager extends AbstractBlockContentManager {
 	int getForceBreakCount() {
 		return forceCount;
 	}
+	
+	private static class AggregatedBrailleTranslatorResult implements BrailleTranslatorResult {
+		
+		private final List<BrailleTranslatorResult> results;
+		private BrailleTranslatorResult current;
+		private int currentIndex = 0;
+		
+		public AggregatedBrailleTranslatorResult(List<BrailleTranslatorResult> results) {
+			this.results = results;
+			this.current = results.get(0);
+			this.currentIndex = 0;
+		}
 
+		public String nextTranslatedRow(int limit, boolean force) {
+			String row = "";
+			while (limit > row.length()) {
+				row += current.nextTranslatedRow(limit - row.length(), force);
+				if (!current.hasNext() && currentIndex + 1 < results.size()) {
+					current = results.get(++currentIndex);
+				} else {
+					break;
+				}
+			}
+			return row;
+		}
+
+		public String getTranslatedRemainder() {
+			String remainder = "";
+			for (int i = currentIndex; i < results.size(); i++) {
+				remainder += results.get(i).getTranslatedRemainder();
+			}
+			return remainder;
+		}
+
+		public int countRemaining() {
+			int remaining = 0;
+			for (int i = currentIndex; i < results.size(); i++) {
+				remaining += results.get(i).countRemaining();
+			}
+			return remaining;
+		}
+
+		public boolean hasNext() {
+			if (current.hasNext()) {
+				return true;
+			} else {
+				while (currentIndex + 1 < results.size()) {
+					current = results.get(++currentIndex);
+					if (current.hasNext()) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		
+		public boolean supportsMetric(String metric) {
+			return METRIC_FORCED_BREAK.equals(metric)
+					|| METRIC_HYPHEN_COUNT.equals(metric);
+		}
+
+		public double getMetric(String metric) {
+			if (METRIC_FORCED_BREAK.equals(metric)
+					|| METRIC_HYPHEN_COUNT.equals(metric)) {
+				int count = 0;
+				for (int i = currentIndex; i >= 0; i--) {
+					count += results.get(i).getMetric(metric);
+				}
+				return count;
+			} else {
+				throw new UnsupportedMetricException("Metric not supported: " + metric);
+			}
+		}
+	}
 }

dotify.formatter.impl/test/org/daisy/dotify/engine/impl/TakenFromDP2Test.java
@@ -21,7 +21,6 @@ public class TakenFromDP2Test extends AbstractFormatterEngineTest {
 		testPEF("resource-files/dp2/page-number-reference-other-sequence-input.obfl",
 		        "resource-files/dp2/page-number-reference-other-sequence-expected.pef", false);
 	}
-	@Ignore // issue with leader (https://github.com/joeha480/obfl/issues/31)
 	@Test
 	public void testPageNumberFollowsLeader() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/dp2/page-number-follows-leader-input.obfl",

dotify.formatter.impl/test/org/daisy/dotify/engine/impl/resource-files/dp2/page-number-follows-leader-expected.pef
@@ -3,7 +3,7 @@
    <head>
       <meta>
          <dc:title xmlns:dc="http://purl.org/dc/elements/1.1/">page-number-follows-leader</dc:title>
-         <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/">Tests leader followed by page-number. Broken: see https://github.com/braillespecs/obfl/issues/31.</dc:description>
+         <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/">Tests leader followed by page-number.</dc:description>
          <dc:format xmlns:dc="http://purl.org/dc/elements/1.1/">application/x-pef+xml</dc:format>
       </meta>
    </head>

dotify.formatter.impl/test/org/daisy/dotify/engine/impl/resource-files/dp2/page-number-follows-leader-input.obfl
@@ -2,7 +2,7 @@
 <obfl xmlns="http://www.daisy.org/ns/2011/obfl" version="2011-1" xml:lang="und" hyphenate="false">
    <meta>
       <dc:title xmlns:dc="http://purl.org/dc/elements/1.1/">page-number-follows-leader</dc:title>
-      <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/">Tests leader followed by page-number. Broken: see https://github.com/braillespecs/obfl/issues/31.</dc:description>
+      <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/">Tests leader followed by page-number.</dc:description>
    </meta>
    <layout-master name="a" duplex="false" page-width="10" page-height="25">
       <default-template>

commit d401c50b9b98b7edff5163c5a264bce4bd31afdb

Fixes #190 which is a bug with anchor/marker possibly being rendered on the wrong row/page introduced in commit 921d38f

dotify.formatter.impl/src/org/daisy/dotify/formatter/impl/BlockContentManager.java
@@ -139,21 +139,13 @@ class BlockContentManager extends AbstractBlockContentManager {
 				case Marker:
 				{
 					Marker m = (Marker)s;
-					if (rows.isEmpty()) {
-						groupMarkers.add(m);
-					} else {
-						rows.peek().addMarker(m);
-					}
+					applyAfterLeader(m);
 					break;
 				}
 				case Anchor:
 				{
 					AnchorSegment as = (AnchorSegment)s;
-					if (rows.isEmpty()) {
-						groupAnchors.add(as.getReferenceID());
-					} else {
-						rows.peek().addAnchor(as.getReferenceID());
-					}
+					applyAfterLeader(as);
 					break;
 				}
 			}
@@ -170,18 +162,24 @@ class BlockContentManager extends AbstractBlockContentManager {
 		}
 	}
 	
-	private List<BrailleTranslatorResult> layoutAfterLeader = null;
+	// List of BrailleTranslatorResult or Marker or AnchorSegment
+	private List<Object> layoutOrApplyAfterLeader = null;
 	private String currentLeaderMode = null;
+	private boolean seenSegmentAfterLeader = false;
 	
 	private void layoutAfterLeader(Translatable spec, String mode) {
 		if (currentLeader!=null) {
-			if (layoutAfterLeader == null) {
-				layoutAfterLeader = new ArrayList<BrailleTranslatorResult>();
-				// use the mode of the first following segment to translate the leader pattern
-				currentLeaderMode = mode;
+			if (layoutOrApplyAfterLeader == null) {
+				layoutOrApplyAfterLeader = new ArrayList<Object>();
+				// use the mode of the first following segment to translate the leader pattern (or
+				// the mode of the first preceding segment)
+				if (!seenSegmentAfterLeader) {
+					currentLeaderMode = mode;
+					seenSegmentAfterLeader = true;
+				}
 			}
 			try {
-				layoutAfterLeader.add(fcontext.getTranslator(mode).translate(spec));
+				layoutOrApplyAfterLeader.add(fcontext.getTranslator(mode).translate(spec));
 			} catch (TranslationException e) {
 				throw new RuntimeException(e);
 			}
@@ -190,13 +188,45 @@ class BlockContentManager extends AbstractBlockContentManager {
 		}
 	}
 	
+	private void applyAfterLeader(final Marker marker) {
+		if (currentLeader!=null) {
+			if (layoutOrApplyAfterLeader == null) {
+				layoutOrApplyAfterLeader = new ArrayList<Object>();
+			}
+			layoutOrApplyAfterLeader.add(marker);
+		} else {
+			if (rows.isEmpty()) {
+				groupMarkers.add(marker);
+			} else {
+				rows.peek().addMarker(marker);
+			}
+		}
+	}
+	
+	private void applyAfterLeader(final AnchorSegment anchor) {
+		if (currentLeader!=null) {
+			if (layoutOrApplyAfterLeader == null) {
+				layoutOrApplyAfterLeader = new ArrayList<Object>();
+			}
+			layoutOrApplyAfterLeader.add(anchor);
+		} else {
+			if (rows.isEmpty()) {
+				groupAnchors.add(anchor.getReferenceID());
+			} else {
+				rows.peek().addAnchor(anchor.getReferenceID());
+			}
+		}
+	}
+	
 	private void layoutLeader() {
 		if (currentLeader!=null) {
 			// layout() sets currentLeader to null
-			if (layoutAfterLeader == null) {
+			if (layoutOrApplyAfterLeader == null) {
 				layout("", null, null);
 			} else {
-				layout(new AggregatedBrailleTranslatorResult(layoutAfterLeader), currentLeaderMode);
+				layout(new AggregatedBrailleTranslatorResult(layoutOrApplyAfterLeader), currentLeaderMode);
+				layoutOrApplyAfterLeader = null;
+				seenSegmentAfterLeader = false;
 			}
 		}
 	}
@@ -343,6 +373,11 @@ class BlockContentManager extends AbstractBlockContentManager {
 			m.row.setLeaderSpace(m.row.getLeaderSpace()+tabSpace.length());
 			rows.add(m.row);
 		}
+		if (btr instanceof AggregatedBrailleTranslatorResult) {
+			AggregatedBrailleTranslatorResult abtr = ((AggregatedBrailleTranslatorResult)btr);
+			abtr.addMarkers(m.row);
+			abtr.addAnchors(m.row);
+		}
 	}
 	
 	private static int getLeaderAlign(Leader leader, int length) {
@@ -385,33 +420,53 @@ class BlockContentManager extends AbstractBlockContentManager {
 	
 	private static class AggregatedBrailleTranslatorResult implements BrailleTranslatorResult {
 		
-		private final List<BrailleTranslatorResult> results;
-		private BrailleTranslatorResult current;
+		private final List<Object> results;
 		private int currentIndex = 0;
 		
-		public AggregatedBrailleTranslatorResult(List<BrailleTranslatorResult> results) {
+		public AggregatedBrailleTranslatorResult(List<Object> results) {
 			this.results = results;
-			this.current = results.get(0);
-			this.currentIndex = 0;
 		}
 
 		public String nextTranslatedRow(int limit, boolean force) {
 			String row = "";
+			BrailleTranslatorResult current = computeNext();
 			while (limit > row.length()) {
 				row += current.nextTranslatedRow(limit - row.length(), force);
-				if (!current.hasNext() && currentIndex + 1 < results.size()) {
-					current = results.get(++currentIndex);
-				} else {
+				current = computeNext();
+				if (current == null) {
 					break;
 				}
 			}
 			return row;
 		}
 
+		private BrailleTranslatorResult computeNext() {
+			while (currentIndex < results.size()) {
+				Object o = results.get(currentIndex);
+				if (o instanceof BrailleTranslatorResult) {
+					BrailleTranslatorResult current = ((BrailleTranslatorResult)o);
+					if (current.hasNext()) {
+						return current;
+					}
+				} else if (o instanceof Marker) {
+					pendingMarkers.add((Marker)o);
+				} else if (o instanceof AnchorSegment) {
+					pendingAnchors.add((AnchorSegment)o);
+				} else {
+					throw new RuntimeException("coding error");
+				}
+				currentIndex++;
+			}
+			return null;
+		}
+
 		public String getTranslatedRemainder() {
 			String remainder = "";
 			for (int i = currentIndex; i < results.size(); i++) {
-				remainder += results.get(i).getTranslatedRemainder();
+				Object o = results.get(i);
+				if (o instanceof BrailleTranslatorResult) {
+					remainder += ((BrailleTranslatorResult)o).getTranslatedRemainder();
+				}
 			}
 			return remainder;
 		}
@@ -419,31 +474,41 @@ class BlockContentManager extends AbstractBlockContentManager {
 		public int countRemaining() {
 			int remaining = 0;
 			for (int i = currentIndex; i < results.size(); i++) {
-				remaining += results.get(i).countRemaining();
+				Object o = results.get(i);
+				if (o instanceof BrailleTranslatorResult) {
+					remaining += ((BrailleTranslatorResult)o).countRemaining();
+				}
 			}
 			return remaining;
 		}
 
 		public boolean hasNext() {
-			if (current.hasNext()) {
-				return true;
-			} else {
-				while (currentIndex + 1 < results.size()) {
-					current = results.get(++currentIndex);
-					if (current.hasNext()) {
-						return true;
-					}
-				}
-			}
-			return false;
+			return computeNext() != null;
 		}
 		
+		List<Marker> pendingMarkers = new ArrayList<Marker>();
+		private void addMarkers(RowImpl row) {
+			for (Marker m : pendingMarkers)
+				row.addMarker(m);
+			pendingMarkers.clear();
+		}
+
+		List<AnchorSegment> pendingAnchors = new ArrayList<AnchorSegment>();
+		private void addAnchors(RowImpl row) {
+			for (AnchorSegment a : pendingAnchors)
+				row.addAnchor(a.getReferenceID());
+			pendingAnchors.clear();
+		}
+
 		public boolean supportsMetric(String metric) {
 			// since we cannot assume that the individual results of any metric can be added, we only support the following known cases
 			if (METRIC_FORCED_BREAK.equals(metric) || METRIC_HYPHEN_COUNT.equals(metric)) {
-				for (int i = 0; i <= currentIndex; i++) {
-					if (!results.get(i).supportsMetric(metric)) {
-						return false;
+				for (int i = 0; i <= currentIndex && i < results.size(); i++) {
+					Object o = results.get(i);
+					if (o instanceof BrailleTranslatorResult) {
+						if (!((BrailleTranslatorResult)o).supportsMetric(metric)) {
+							return false;
+						}
 					}
 				}
 				return true;
@@ -456,8 +521,11 @@ class BlockContentManager extends AbstractBlockContentManager {
 			// since we cannot assume that the individual results of any metric can be added, we only support the following known cases
 			if (METRIC_FORCED_BREAK.equals(metric) || METRIC_HYPHEN_COUNT.equals(metric)) {
 				int count = 0;
-				for (int i = 0; i <= currentIndex; i++) {
-					count += results.get(i).getMetric(metric);
+				for (int i = 0; i <= currentIndex && i < results.size(); i++) {
+					Object o = results.get(i);
+					if (o instanceof BrailleTranslatorResult) {
+						count += ((BrailleTranslatorResult)o).getMetric(metric);
+					}
 				}
 				return count;
 			} else {

dotify.formatter.impl/test/org/daisy/dotify/engine/impl/MarkerReferenceTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.daisy.dotify.api.engine.LayoutEngineException;
 import org.daisy.dotify.api.writer.PagedMediaWriterConfigurationException;
-import org.junit.Ignore;
 import org.junit.Test;
 public class MarkerReferenceTest extends AbstractFormatterEngineTest {
 
@@ -33,7 +32,7 @@ public class MarkerReferenceTest extends AbstractFormatterEngineTest {
 		testPEF("resource-files/marker-ref-sheet-input.obfl", "resource-files/marker-ref-sheet-expected.pef", false);
 	}
 	
-	@Test @Ignore //see https://github.com/joeha480/dotify/issues/190
+	@Test
 	public void testMarkerAfterLeader() throws LayoutEngineException, IOException, PagedMediaWriterConfigurationException {
 		testPEF("resource-files/marker-ref-after-leader-input.obfl", "resource-files/marker-ref-after-leader-expected.pef", false);
 	}